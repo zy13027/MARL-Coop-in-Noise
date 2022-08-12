269ed85e608e2c562c315d4d9a02230c21220a46
zy13027 <68322026+zy13027@users.noreply.github.com>
2022-08-13 00:30:10 +0800

    Add files via upload

---
+++ ddc2bi.m
@@ -0,0 +1,9 @@
+function vecargout = dc2bi(intgargin)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+biNum = dec2bin(intgargin);
+ emptyVec = zeros(4,1);
+
+
+
+end
